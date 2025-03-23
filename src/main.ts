cdabaa789ab282442bb20b695a26cd76e61c0cc2
ciringa <thierrird957468@gmail.com>
2025-03-23 13:01:09 -0300

    docs: rest.http api test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HOST, PORT } from './shared/lib/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(PORT ?? 3000,HOST,()=>{
+    console.log(`\n ClearFaceAPI running on ${HOST} port ${process.env.PORT ?? 3000} full adress \u001b[34m http://${HOST}:${process.env.PORT ?? 3000} \u001b[0m`);
+  });
 }
 bootstrap();
--- src/shared/lib/env/index.ts
+++ src/shared/lib/env/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import { z } from "zod";
 
-export const {ADMIN_EMAIL,ADMIN_PASSWORD,JWT_SECRET} = z.object({
+export const {ADMIN_EMAIL,ADMIN_PASSWORD,JWT_SECRET,HOST,PORT} = z.object({
     ADMIN_EMAIL: z.string(),
     ADMIN_PASSWORD: z.string(),
     JWT_SECRET: z.string(),
+    PORT:z.string(),
+    HOST:z.string()
 }).parse(process.env)
