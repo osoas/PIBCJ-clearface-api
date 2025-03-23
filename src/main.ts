5a9ce2a0ab23411ee8fa2ba532b2318152ba5bf0
ciringa <thierrird957468@gmail.com>
2025-03-23 15:03:37 -0300

    docs: swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HOST, PORT } from './shared/lib/env';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { swaggerDocument } from './types/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +11,19 @@ async function bootstrap() {
 
   app.enableCors();
 
+  //Setup do swagger
+  const config = new DocumentBuilder()
+    .setTitle('ClearFaceAPI')
+    .setDescription('ClearFaceAPI - a face recognition plataform api for acne detection and classification')
+    .setVersion('1.0')
+    .addTag('ClearFaceAPI')
+    .build();
+  //const document = SwaggerModule.createDocument(app, config); //it will create an atomatic documentation based on the config but less customizable
+  
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
   await app.listen(PORT ?? 3000,HOST,()=>{
-    console.log(`\n ClearFaceAPI running on ${HOST} port ${process.env.PORT ?? 3000} full adress \u001b[34m http://${HOST}:${process.env.PORT ?? 3000} \u001b[0m`);
+    console.log(`\nClearFaceAPI running on ${HOST} port ${process.env.PORT ?? 3000} full adress \u001b[34m http://${HOST}:${process.env.PORT ?? 3000} \u001b[0m\ncheck documentation \u001b[34m http://${HOST}:${process.env.PORT ?? 3000}/docs \u001b[0m\n`);
   });
 }
 bootstrap();
